9a4b4647d24a57cb2ce2e9bd87e49d1ef125efe8
Natalia Vdovina <nat12-09@mail.ru>
2019-10-30 09:10:23 +0300

    task 4: refactor db config

--- src/main/java/com/spring/project/config/DbConfig.java
+++ src/main/java/com/spring/project/config/DbConfig.java
@@ -1,27 +1,27 @@
 package com.spring.project.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
-@ComponentScan("com.spring.project")
-public class EmbeddedDatabaseConfig {
+@Configuration
+public class DbConfig {
 
     @Bean
-    public DataSource dataSource(){
+    public DataSource dataSource() {
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
-                .addScript("classpath:/Schema.sql")
-                .addScript("classpath:/test-data.sql")
+                .addScript("classpath:schema.sql")
+                .addScript("classpath:test-data.sql")
                 .build();
     }
 
     @Bean
-    public JdbcTemplate jdbcTemplate(){
+    public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource());
     }
 }
